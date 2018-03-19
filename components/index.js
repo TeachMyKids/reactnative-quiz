c49ab40761e7b8b1ba4cba9f08be4cd4db38ca96
Binh Quan <binhqd@gmail.com>
2018-03-19 15:50:05 +0700

    Initial code

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import MathContainer from './MathContainer';
+import Question from './Question';
+import Answer from './Answer';
+import Keyboard from './Keyboard.js';
+
+export { MathContainer, Question, Answer, Keyboard };
