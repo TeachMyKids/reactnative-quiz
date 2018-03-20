675f4f09ca45ad422dc5e97af7102d1e1c2f4c12
Binh Quan <binhqd@gmail.com>
2018-03-20 10:14:33 +0700

    Update button

--- components/Keyboard.js
+++ components/Keyboard.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import Dimensions from 'Dimensions';
 let width = Dimensions.get('window').width;
+
+function BtnNumber(props) {
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+        <View style={props.style}>
+          <Text style={props.textStyle}>{props.title}</Text>
+        </View>
+    </TouchableOpacity>
+  );
+}
 
 class Keyboard extends React.Component {
   constructor(props, context) {
@@ -48,56 +58,56 @@ class Keyboard extends React.Component {
     return (
       <View style={ styles.main }>
         <View style={ styles.row }>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 1)} title="1"/>
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 1)} style={ styles.button } textStyle={ styles.btnTextStyle } title="1"/>
           </View>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 2)} title="2" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 2)} style={ styles.button } textStyle={ styles.btnTextStyle } title="2" />
           </View>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 3)} title="3" />
-          </View>
-        </View>
-
-        <View style={ styles.row }>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 4)} title="4" />
-          </View>
-
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 5)} title="5" />
-          </View>
-
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 6)} title="6" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 3)} style={ styles.button } textStyle={ styles.btnTextStyle } title="3" />
           </View>
         </View>
 
         <View style={ styles.row }>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 7)} title="7" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 4)} style={ styles.button } textStyle={ styles.btnTextStyle } title="4" />
           </View>
 
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 8)} title="8" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 5)} style={ styles.button } textStyle={ styles.btnTextStyle } title="5" />
           </View>
 
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 9)} title="9" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 6)} style={ styles.button } textStyle={ styles.btnTextStyle } title="6" />
           </View>
         </View>
 
         <View style={ styles.row }>
-          <View style={ styles.button }>
-            <Button onPress={this.onNumberEnter.bind(this, 0)} title="0" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 7)} style={ styles.button } textStyle={ styles.btnTextStyle } title="7" />
           </View>
 
-          <View style={ styles.button }>
-            <Button onPress={this.props.onAnswer} color='green' title="=" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 8)} style={ styles.button } textStyle={ styles.btnTextStyle } title="8" />
           </View>
 
-          <View style={ styles.button }>
-            <Button onPress={this.back} color='orange' title="<=" />
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 9)} style={ styles.button } textStyle={ styles.btnTextStyle } title="9" />
+          </View>
+        </View>
+
+        <View style={ styles.row }>
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.onNumberEnter.bind(this, 0)} style={ styles.button } textStyle={ styles.btnTextStyle } title="0" />
+          </View>
+
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.props.onAnswer} style={[ styles.button, styles.green ]} textStyle={ styles.btnTextStyle } title="=" />
+          </View>
+
+          <View style={ styles.buttonContainer }>
+            <BtnNumber onPress={this.back} style={[ styles.button, styles.red ]} textStyle={ styles.btnTextStyle } title="<=" />
           </View>
         </View>
       </View>
@@ -123,9 +133,26 @@ let styles = StyleSheet.create({
   back: {
     // backgroundColor: '#ff0000',
   },
-  button: {
+  buttonContainer: {
     margin: 5,
     width: (width / 3 - 10) / 6 * 5
+  },
+  button: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    height: 45
+  },
+  green: {
+    backgroundColor: 'green'
+  },
+  btnTextStyle: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  red: {
+    backgroundColor: 'red'
   }
 });
 
