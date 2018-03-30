dce8cb908fed23f0db65cf930db88ff1d51006f9
Binh Quan <binhqd@gmail.com>
2018-03-30 14:08:44 +0700

    Add input for receive multiple values without concern about their order

---
+++ components/user-inputs/SelectOrderedValues.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Dimensions from 'Dimensions';
+
+import MyButton from './Button';
+
+let width = Dimensions.get('window').width;
+
+class SelectOrderedValues extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      inputLength: this.props.expectedAnswer.length,
+      currentFieldIndex: 0,
+      answers: this.props.answers.sort(function(a, b){return 0.5 - Math.random()}),
+      answer: new Array(this.props.expectedAnswer.length),
+      inputState: this.props.inputState
+    };
+
+    this.onButtonPress = this.onButtonPress.bind(this);
+    this.onAnswer = this.onAnswer.bind(this);
+  }
+
+  _arrayEquals(a, b) {
+    if (!Array.isArray(a) || !Array.isArray(b)) return false;
+    if (a.length != b.length) return false;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  componentDidMount() {
+    this.props.onRef(this);
+
+    this.isComponentDidMounted = true;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.answers !== this.state.answers) {
+      this.setState({
+        answers: nextProps.answers.sort(function(a, b){return 0.5 - Math.random()}),
+        inputLength: nextProps.expectedAnswer.length
+      })
+    }
+
+    if (nextProps.inputState !== this.state.inputState) {
+      this.setState({
+        inputState: nextProps.inputState,
+        currentFieldIndex: nextProps.inputState.fillInBlank.fieldIndex
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.isComponentDidMounted = false;
+  }
+
+  reset() {
+    if (this.isComponentDidMounted) {
+      this.setState({
+        answer: new Array(this.state.inputLength),
+        currentFieldIndex: 0
+      });
+    }
+  }
+
+  onButtonPress(answer) {
+    let newAnswer = [
+      ...this.state.answer
+    ];
+    newAnswer[this.state.currentFieldIndex] = answer;
+
+    this.setState({
+      answer: newAnswer
+    });
+
+    this.props.onResultChange(newAnswer);
+  }
+
+  onAnswer() {
+    this.props.onAnswer(this._arrayEquals(this.state.answer, this.props.expectedAnswer), this.state.answer);
+  }
+
+  render() {
+    let buttonWidth = (width / this.props.answers.length) - 10; // subtract the padding by 10
+
+    return (
+      <View style={ styles.main }>
+        <View style={ styles.row }>
+          {this.state.answers.map((answer, index) => {
+            return (
+              <View style={[ styles.buttonContainer ]} key={index}>
+                <MyButton
+                  onPress={() => this.onButtonPress(answer.value)}
+                  style={ styles.button }
+                  textStyle={ styles.btnTextStyle }
+                  title={answer.text}
+                />
+              </View>
+            )
+          })}
+        </View>
+
+        <View style={ styles.answerContainer }>
+          <MyButton
+            title='Trả Lời'
+            style={ styles.answerButton }
+            textStyle={ styles.answerButtonTextStyle }
+            onPress={() => {
+              this.onAnswer()
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  row: {
+    justifyContent: 'center',
+    // width: width / 2,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  answerButtonTextStyle: {
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 23
+  },
+
+  answerButton: {
+    alignSelf: 'center',
+    margin: 5,
+
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    height: 45,
+    width: 150
+  },
+
+  buttonContainer: {
+    margin: 5
+  },
+  button: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    height: 45,
+    padding: 10
+  },
+  btnTextStyle: {
+    fontSize: 20,
+    color: '#fff'
+  }
+});
+
+SelectOrderedValues.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  answers: PropTypes.array.isRequired,
+  expectedAnswer: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default SelectOrderedValues;
