14b34647d8d09bdbb530d73f4948ed04110f9dc4
Binh Quan <binhqd@gmail.com>
2018-04-16 11:10:42 +0700

    Add Firebase Firestore

--- android/app/src/main/java/com/mathquiz/MainApplication.java
+++ android/app/src/main/java/com/mathquiz/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 
 import java.util.Arrays;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNFirebasePackage()
+          new RNFirebasePackage(),
+          new RNFirebaseFirestorePackage()
       );
     }
 
